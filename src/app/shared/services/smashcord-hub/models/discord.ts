f053d95b9e7375b12d025df2e0884305a5a26595
Valentin Michalak <valentinmichalak@gmail.com>
2020-10-24 19:32:51 +0200

    Add discord icons.

--- src/app/shared/services/smashcord-hub/models/discord.ts
+++ src/app/shared/services/smashcord-hub/models/discord.ts
@@ -1,6 +1,7 @@
 export class Discord {
   constructor(
     public readonly name: string,
+    public readonly icon: string,
     public readonly admins: string[],
     public readonly invitationId: string,
   ) {}
